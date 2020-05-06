b8f20d8a18d71eaad4e4da548e30b54aca5e06da
Marco Perone <pasafama@gmail.com>
2020-05-06 18:40:56 +0200

    spock plaintext endpoint

--- spock/app/Main.hs
+++ spock/app/Main.hs
@@ -4,25 +4,12 @@ module Main where
 import Web.Spock
 import Web.Spock.Config
 
-import Control.Monad.Trans
-import Data.Monoid
-import Data.IORef
-import qualified Data.Text as T
-
-data MySession = EmptySession
-data MyAppState = DummyAppState (IORef Int)
-
 main :: IO ()
-main =
-    do ref <- newIORef 0
-       spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
-       runSpock 8080 (spock spockCfg app)
+main = do
+  spockCfg <- defaultSpockCfg () PCNoDatabase ()
+  runSpock 8080 (spock spockCfg app)
 
-app :: SpockM () MySession MyAppState ()
-app =
-    do get root $
-           text "Hello World!"
-       get ("hello" <//> var) $ \name ->
-           do (DummyAppState ref) <- getState
-              visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
-              text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+app :: SpockM () () () ()
+app = do
+  get "plaintext" $
+    text "Hello World!"
